4ef4b2cfb77c41b59caaaafc8d21fa0fc652b36f
Devin Lehmacher <lehmacdj@gmail.com>
2016-10-19 00:56:22 -0400

    Update bst interface slightly

--- bst.h
+++ bst.h
@@ -18,22 +18,26 @@ struct BST* create_bst(int k, int v);
 
 /**
  * Destroy [bst].
+ * Precondition bst != NULL.
  */
 void destroy_bst(struct BST* bst);
 
 /**
  * Return the value associated with [k] in [bst]. If [k] is not in [bst]
  * behavior is undefined.
+ * Precondition bst != NULL.
  */
 int bst_search(struct BST* bst, int k);
 
 /**
  * Insert [(k, v)] into [bst]. If [k] is already in [bst] behavior is undefined.
+ * Precondition bst != NULL.
  */
 void bst_insert(struct BST* bst, int k, int v);
 
 /**
  * Delete the key and value associated with [k] from [bst]. If [k] is not in
  * [bst] do nothing.
+ * Precondition bst != NULL.
  */
 void bst_delete(struct BST* bst, int k);
